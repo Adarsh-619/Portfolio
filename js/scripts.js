0297802c1c570afe60787aaf3d29e793d2cae256
Adarsh Patra <patra.adarsh9@gmail.com>
2024-01-25 20:22:10 +0530

    Added the index.html and script.js

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-unused-vars */
+function setupTypewriter(t) {
+  const HTML = t.innerHTML;
+  t.innerHTML = '';
+  let cursorPosition = 0;
+  let tag = '';
+  let writingTag = false;
+  let tagOpen = false;
+  const typeSpeed = 100;
+  let tempTypeSpeed = 0;
+
+  const type = function () {
+    if (writingTag === true) {
+      tag += HTML[cursorPosition];
+    }
+
+    if (HTML[cursorPosition] === '<') {
+      tempTypeSpeed = 0;
+      if (tagOpen) {
+        tagOpen = false;
+        writingTag = true;
+      } else {
+        tag = '';
+        tagOpen = true;
+        writingTag = true;
+        tag += HTML[cursorPosition];
+      }
+    }
+    if (!writingTag && tagOpen) {
+      tag.innerHTML += HTML[cursorPosition];
+    }
+    if (!writingTag && !tagOpen) {
+      if (HTML[cursorPosition] === ' ') {
+        tempTypeSpeed = 0;
+      } else {
+        tempTypeSpeed = Math.random() * typeSpeed + 50;
+      }
+      t.innerHTML += HTML[cursorPosition];
+    }
+    if (writingTag === true && HTML[cursorPosition] === '>') {
+      tempTypeSpeed = Math.random() * typeSpeed + 50;
+      writingTag = false;
+      if (tagOpen) {
+        const newSpan = document.createElement('span');
+        t.appendChild(newSpan);
+        newSpan.innerHTML = tag;
+        tag = newSpan.firstChild;
+      }
+    }
+
+    cursorPosition += 1;
+    if (cursorPosition < HTML.length - 1) {
+      setTimeout(type, tempTypeSpeed);
+    }
+  };
+
+  return {
+    type: type,
+  };
+}
+const typer = document.getElementById('typewriter');
+// eslint-disable-next-line no-undef
+typewriter = setupTypewriter(typewriter);
+// eslint-disable-next-line no-undef
+typewriter.type();
+
+const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+darkModeMediaQuery.addEventListener('change', () => {
+  setColorSheme();
+});
+
+function setColorSheme() {
+  const navbar = document.getElementById('navbar');
+  const useDark = window.matchMedia('(prefers-color-scheme: dark)');
+  if (useDark.matches === true) {
+    navbar.classList.remove('navbar-light');
+    navbar.classList.add('navbar-dark');
+  } else {
+    navbar.classList.add('navbar-light');
+    navbar.classList.remove('navbar-dark');
+  }
+}
+setColorSheme();
